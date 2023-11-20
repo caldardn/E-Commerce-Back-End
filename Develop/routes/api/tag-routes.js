5fb7ca3c149eee3eac5582279d1248b41152a576
caldardn <caldardn@gmail.com>
2023-11-19 22:30:37 -0500

    adding get code to find a single tag by it's id

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
         }
       ]
     });
-    
+
     res.json(allTags);
 
   }catch(err){
@@ -28,9 +28,31 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagFind = await Tag.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'tag_name'],
+      include: [
+        {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+      ]
+    });
+    if(!tagFind){
+      res.status(404).json({message: 'No tag found with this id!'});
+      return;
+    }
+    res.json(tagFind);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
